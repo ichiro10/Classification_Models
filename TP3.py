08ce9ce73c089e64e1b13dd698c708ef09796bd9
ichiro10 <ghammaz.ali1@gmail.com>
2023-11-11 03:19:25 -0600

    piplines

--- TP3.py
+++ TP3.py
@@ -8,40 +8,25 @@ import scipy.stats as stats
 from sklearn.preprocessing import QuantileTransformer
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
 import sys
+from sklearn.compose import make_column_selector
+from sklearn.compose import make_column_transformer
 
-
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
 from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import cross_validate
+from sklearn.linear_model import LogisticRegression
+from sklearn.svm import SVC
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.ensemble import RandomForestClassifier, GradientBoostingClassifier
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import make_pipeline
+from sklearn.compose import ColumnTransformer
 
-
-
-def predictionVStrue(y_true, y_pred, mse, r2 ,name ,strategy):
-    plt.scatter(y_true, y_pred, label=f'{name},{strategy}\nMSE: {mse:.2f}, R-squared: {r2:.2f}')
-    plt.plot([min(y_true), max(y_true)], [min(y_true), max(y_true)], linestyle='--', color='red', label='Perfect Prediction')
-    plt.xlabel("True Values")
-    plt.ylabel("Predictions")
-    plt.legend()
-    plt.title("Regression Model Predictions")
-    plt.show()    
-
-
-def residual_plot(y_true, y_pred, model_name):
-        plt.plot(y_pred, y_true - y_pred, "*")
-        plt.plot(y_pred, np.zeros_like(y_pred), "-")
-        plt.legend(["Data", "Perfection"])
-        plt.title("Residual Plot of " + model_name)
-        plt.xlabel("Predicted Value")
-        plt.ylabel("Residual")
-        plt.show()   
-
-
-def Preparing_models(X_train, X_test, y_train):
-        # Créez une liste pour stocker les modèles
-        models = []
-        
-
-        return models
 
 def data_preprocessing(data):
         print(data)
@@ -49,7 +34,6 @@ def data_preprocessing(data):
         print(data.nunique())
         print(data.describe(include='all'))
 
-        #data_viz(data)
         #missed values 
         print(data.isnull().sum())
 
@@ -65,32 +49,6 @@ def data_preprocessing(data):
         print(data.info())
         print(data.describe(include='all'))
         
-            
-        #outliers 
-        if False:
-            qt = QuantileTransformer(output_distribution='normal', n_quantiles=100)
-
-            for col in data.columns:
-                data[col] = data[col] = qt.fit_transform(pd.DataFrame(data[col]))
-            
-
-            for col in data:
-                    q1 = data[col].quantile(0.25)
-                    q3 = data[col].quantile(0.75)
-                    iqr = q3 - q1
-                    whisker_width = 1.5
-                    lower_whisker = q1 - (whisker_width * iqr)
-                    upper_whisker = q3 + whisker_width * iqr
-                    data[col] = np.where(data[col] > upper_whisker, upper_whisker, np.where(data[col] < lower_whisker, lower_whisker, data[col]))
-        
-        params = data.drop('Credit Score', axis=1)
-        target = data['Credit Score']
-
-        scalar = StandardScaler()
-        scalar.fit(params)
-        scaled_inputs = scalar.transform(params)
-
-        return scaled_inputs, target
 
 def data_viz(data):
       
@@ -102,11 +60,10 @@ def data_viz(data):
             plt.title(column)
             plt.show()    
         
-
-        
         for column in d:
                 sns.boxplot(data = data, x = column)
                 plt.show()    
+
         features_list = [feature for feature in data.columns.tolist() if feature != 'Credit Score']
 
         params = data[features_list]
@@ -120,68 +77,108 @@ def run(csv: str = './CS_Dataset.csv'):
         data = pd.read_csv(csv)
         print(data)
         print(data.info())
-        print(data.nunique())
-        print(data.describe(include='all'))
 
-        #data_viz(data)
-        #missed values 
-        print(data.isnull().sum())
+        X = data.drop('Credit Score', axis=1)
+        y = data['Credit Score']
+  
 
-        #categorical data 
-        categorical_features = ['Gender','Education','Marital Status','Home Ownership'] 
+        # Split the data
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-        for feature in categorical_features:
-            # Label Encoding
-            label_encoder = LabelEncoder()
-            data[feature+'_encoded'] = label_encoder.fit_transform(data[feature])
-            data = data.drop(columns=[feature])
-        print(data)
-        print(data.info())
-        print(data.describe(include='all'))
-        
+        # Define numeric and categorical features
+        numeric_features = X.select_dtypes(include=['int64']).columns
+        categorical_features = X.select_dtypes(include=['object']).columns
 
-        d = data.drop(columns=['Credit Score'])
+        # Create transformers for numeric and categorical features
+        numeric_transformer = Pipeline(steps=[
+            ('scaler', StandardScaler())
+        ])
 
-        for column in d.columns : 
-            plt.figure(figsize = (14,4))
-            sns.histplot(data[column])
-            plt.title(column)
-            plt.show()    
-        
+        categorical_transformer = Pipeline(steps=[
+            ('onehot', OneHotEncoder())
+        ])
 
-        
-        for column in d:
-                sns.boxplot(data = data, x = column)
-                plt.show()    
-        features_list = [feature for feature in data.columns.tolist() if feature != 'Credit Score']
+        # Combine transformers using ColumnTransformer
+        preprocessor = ColumnTransformer(
+            transformers=[
+                ('num', numeric_transformer, numeric_features),
+                ('cat', categorical_transformer, categorical_features)
+            ])
 
-        params = data[features_list]
-        plt.figure(figsize=(10,10))
-        sns.set_theme()
-        sns.heatmap(params.corr(),annot=True, fmt="0.1g", cmap='PiYG')
-        plt.show()           
-        
+        # Create a pipeline with the preprocessor and a classifier (Random Forest in this case)
+        classifier = Pipeline(steps=[
+            ('preprocessor', preprocessor),
+            ('classifier', RandomForestClassifier())
+        ])
+
+        # Fit the pipeline on the training data
+        classifier.fit(X_train, y_train)
+
+        # Predictions on the test set
+        y_pred = classifier.predict(X_test)
+
+        # Evaluate the model
+        # You can use metrics like accuracy, precision, recall, etc.
+        from sklearn.metrics import accuracy_score
+        accuracy = accuracy_score(y_test, y_pred)
+        print(f'Accuracy: {accuracy}')
+        if False:
+            numerical_features = ['Age', 'Income', 'Number of Children']
+            categorical_features = ['Gender', 'Education', 'Marital Status','Home Ownership']
 
 
-            
+
+            numerical_pipeline = make_pipeline(StandardScaler())
+            categorical_pipeline = make_pipeline(LabelEncoder())
+            # Updated the structure of the ColumnTransformer
+
+            preprocessor = make_column_transformer((numerical_pipeline, numerical_features),
+                                    (categorical_pipeline, categorical_features))
+
+            # List of classifiers
+            clfs = [
+                ('Logistic Regression', LogisticRegression()),
+                ('SVM', SVC()),
+                ('K-Nearest Neighbors', KNeighborsClassifier(n_neighbors=3)),
+                ('Random Forest', RandomForestClassifier()),
+                ('DecisionTree', DecisionTreeClassifier()),
+                ('GradientBoosting', GradientBoostingClassifier())
+            ]
+
+            # Iterate over classifiers
+            for clf_name, clf in clfs:
+                # Define the pipeline
+                model = make_pipeline(
+                    ('preprocessor', preprocessor),
+                    (clf_name, clf)
+                )
+                model.fit(X_train, y_train)
 
 
 
 
 
-if False :
-    plt.figure(figsize=(12, 6))
+            if False:
+                # Cross-validate the model
+                scores = cross_validate(pipeline, X_train, y_train, scoring="accuracy", cv=5)
 
-    scaled_inputs, target = data_preprocessing(data)
-    #split the data
-    X_train, X_test, y_train, y_test = train_test_split(scaled_inputs, target, test_size=0.2, random_state = 42)
+                # Print results
+                print('---------------------------------')
+                print(clf_name)
+                print('-----------------------------------')
+                for key, values in scores.items():
+                    print(key, ' mean ', values.mean())
+                    print(key, ' std ', values.std())
 
-    # Loop through the baseline strategies and evaluate dummy regressors
 
-    models= Preparing_models(X_train, X_test, y_train)
-    for name, model in models:
-        y_pred = model.predict(X_test)
-      
+
+
+
+
+
+ 
+
+
 
 
 
